8ada7eefec39630482b97fffc3ff405f436631cb
Travis Clarke <tclarke@pietech.com>
2016-01-15 12:14:04 -0500

    build and package update

--- config.js
+++ config.js
@@ -13,16 +13,16 @@ System.config({
   },
 
   map: {
-    "babel": "npm:babel-core@5.7.2",
-    "babel-runtime": "npm:babel-runtime@5.7.0",
-    "core-js": "npm:core-js@0.9.18",
+    "babel": "npm:babel-core@5.8.34",
+    "babel-runtime": "npm:babel-runtime@5.8.34",
+    "core-js": "npm:core-js@2.0.3",
     "google/material-design-lite": "github:google/material-design-lite@1.0.6",
     "lodash": "npm:lodash@3.10.1",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
-    "github:jspm/nodelibs-process@0.1.1": {
-      "process": "npm:process@0.10.1"
+    "github:jspm/nodelibs-path@0.1.0": {
+      "path-browserify": "npm:path-browserify@0.0.0"
     },
     "github:jspm/nodelibs-process@0.1.2": {
       "process": "npm:process@0.11.2"
@@ -33,18 +33,22 @@ System.config({
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:babel-runtime@5.7.0": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
+    "npm:babel-runtime@5.8.34": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:core-js@0.9.18": {
+    "npm:core-js@2.0.3": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "process": "github:jspm/nodelibs-process@0.1.1",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.2",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
     "npm:lodash@3.10.1": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:process@0.11.2": {
--- dist/mdl.js
+++ dist/mdl.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, '__esModule', {
-  value: true
+    value: true
 });
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
@@ -10,172 +10,170 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 var _aureliaFramework = require('aurelia-framework');
 
-var _googleMaterialDesignLite = require('google/material-design-lite');
+var _githubGoogleMaterialDesignLite106Material = require('github:google/material-design-lite@1.0.6/material');
 
 var mdlTypes = {
-  button: {
-    html: ["mdl-button", "mdl-js-button"],
-    js: ['MaterialButton'],
-    fct: [manageRipple]
+    button: {
+        html: ["mdl-button", "mdl-js-button"],
+        js: ['MaterialButton'],
+        fct: [manageRipple]
 
-  },
-  textfield: {
-    js: ['MaterialTextfield'],
-    html: ["mdl-textfield", "mdl-js-textfield"]
-  },
-  layout: {
-    js: ['MaterialLayout'],
-    html: ["mdl-layout", "mdl-js-layout"]
-  },
-  menu: {
-    js: ['MaterialMenu'],
-    html: ["mdl-menu", "mdl-js-menu"],
-    fct: [manageRipple]
-  },
-  "data-table": {
-    js: ['MaterialDataTable'],
-    html: ["mdl-data-table", "mdl-js-data-table"],
-    fct: [manageRipple]
-  },
-  tabs: {
-    js: ['MaterialTabs'],
-    html: ["mdl-tabs", "mdl-js-tabs"],
-    fct: [manageRipple]
-  },
-  slider: {
-    js: ['MaterialSlider'],
-    html: ["mdl-slider", "mdl-js-slider"]
-  },
-  tooltip: {
-    js: ['MaterialTooltip'],
-    html: ["mdl-tooltip"]
-  },
-  progress: {
-    js: ['MaterialProgress'],
-    html: ["mdl-progress", "mdl-js-progress"]
-  },
-  spinner: {
-    js: ['MaterialSpinner'],
-    html: ["mdl-spinner", "mdl-js-spinner"]
-  },
-  badge: {
-    html: ["mdl-bagde"]
-  },
-  "switch": {
-    js: ['MaterialSwitch'],
-    html: ["mdl-switch", "mdl-js-switch"],
-    fct: [manageRipple]
-  },
-  "radio": {
-    js: ['MaterialRadio'],
-    html: ["mdl-radio", "mdl-js-radio"],
-    fct: [manageRipple]
-  },
-  "icon-toggle": {
-    js: ['MaterialIconToggle'],
-    html: ["mdl-icon-toggle", "mdl-js-icon-toggle"],
-    fct: [manageRipple]
-  },
-  "checkbox": {
-    js: ['MaterialCheckbox'],
-    html: ["mdl-checkbox", "mdl-js-checkbox"],
-    fct: [manageRipple]
-  }
-
+    },
+    textfield: {
+        js: ['MaterialTextfield'],
+        html: ["mdl-textfield", "mdl-js-textfield"]
+    },
+    layout: {
+        js: ['MaterialLayout'],
+        html: ["mdl-layout", "mdl-js-layout"]
+    },
+    menu: {
+        js: ['MaterialMenu'],
+        html: ["mdl-menu", "mdl-js-menu"],
+        fct: [manageRipple]
+    },
+    "data-table": {
+        js: ['MaterialDataTable'],
+        html: ["mdl-data-table", "mdl-js-data-table"],
+        fct: [manageRipple]
+    },
+    tabs: {
+        js: ['MaterialTabs'],
+        html: ["mdl-tabs", "mdl-js-tabs"],
+        fct: [manageRipple]
+    },
+    slider: {
+        js: ['MaterialSlider'],
+        html: ["mdl-slider", "mdl-js-slider"]
+    },
+    tooltip: {
+        js: ['MaterialTooltip'],
+        html: ["mdl-tooltip"]
+    },
+    progress: {
+        js: ['MaterialProgress'],
+        html: ["mdl-progress", "mdl-js-progress"]
+    },
+    spinner: {
+        js: ['MaterialSpinner'],
+        html: ["mdl-spinner", "mdl-js-spinner"]
+    },
+    badge: {
+        html: ["mdl-badge"]
+    },
+    "switch": {
+        js: ['MaterialSwitch'],
+        html: ["mdl-switch", "mdl-js-switch"],
+        fct: [manageRipple]
+    },
+    "radio": {
+        js: ['MaterialRadio'],
+        html: ["mdl-radio", "mdl-js-radio"],
+        fct: [manageRipple]
+    },
+    "icon-toggle": {
+        js: ['MaterialIconToggle'],
+        html: ["mdl-icon-toggle", "mdl-js-icon-toggle"],
+        fct: [manageRipple]
+    },
+    "checkbox": {
+        js: ['MaterialCheckbox'],
+        html: ["mdl-checkbox", "mdl-js-checkbox"],
+        fct: [manageRipple]
+    }
 };
 
 function manageRipple(element) {
-  if (element.classList.contains("mdl-js-ripple-effect")) _googleMaterialDesignLite.componentHandler.upgradeElement(element, "MaterialRipple");
-  var elts = element.getElementsByClassName(".mdl-js-ripple-effect");
-  for (var i = 0; i < elts.length; i++) {
-    var elt = elts[i];
-    _googleMaterialDesignLite.componentHandler.upgradeElement(elt, "MaterialRipple");
-  }
+    var elts = element.getElementsByClassName(".mdl-js-ripple-effect");
+    for (var i = 0; i < elts.length; i++) {
+        var elt = elts[i];
+        _githubGoogleMaterialDesignLite106Material.componentHandler.upgradeElement(elt, "MaterialRipple");
+    }
 }
 
 function upgradeElement(element, type) {
-  var _ref = mdlTypes[type] || {};
+    var _ref = mdlTypes[type] || {};
 
-  var _ref$fct = _ref.fct;
-  var fct = _ref$fct === undefined ? [] : _ref$fct;
-  var html = _ref.html;
-  var _ref$js = _ref.js;
-  var js = _ref$js === undefined ? [] : _ref$js;
+    var _ref$fct = _ref.fct;
+    var fct = _ref$fct === undefined ? [] : _ref$fct;
+    var html = _ref.html;
+    var _ref$js = _ref.js;
+    var js = _ref$js === undefined ? [] : _ref$js;
 
-  if (html) {
-    for (var i = 0; i < html.length; i++) {
-      element.classList.add(html[i]);
+    if (html) {
+        for (var i = 0; i < html.length; i++) {
+            element.classList.add(html[i]);
+        }
     }
-  }
 
-  var _iteratorNormalCompletion = true;
-  var _didIteratorError = false;
-  var _iteratorError = undefined;
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
 
-  try {
-    for (var _iterator = js[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-      var _type = _step.value;
-      _googleMaterialDesignLite.componentHandler.upgradeElement(element, _type);
-    }
-  } catch (err) {
-    _didIteratorError = true;
-    _iteratorError = err;
-  } finally {
     try {
-      if (!_iteratorNormalCompletion && _iterator['return']) {
-        _iterator['return']();
-      }
+        for (var _iterator = js[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var _type = _step.value;
+            _githubGoogleMaterialDesignLite106Material.componentHandler.upgradeElement(element, _type);
+        }
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
     } finally {
-      if (_didIteratorError) {
-        throw _iteratorError;
-      }
+        try {
+            if (!_iteratorNormalCompletion && _iterator['return']) {
+                _iterator['return']();
+            }
+        } finally {
+            if (_didIteratorError) {
+                throw _iteratorError;
+            }
+        }
     }
-  }
 
-  var _iteratorNormalCompletion2 = true;
-  var _didIteratorError2 = false;
-  var _iteratorError2 = undefined;
+    var _iteratorNormalCompletion2 = true;
+    var _didIteratorError2 = false;
+    var _iteratorError2 = undefined;
 
-  try {
-    for (var _iterator2 = fct[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-      var f = _step2.value;
-      f(element);
-    }
-  } catch (err) {
-    _didIteratorError2 = true;
-    _iteratorError2 = err;
-  } finally {
     try {
-      if (!_iteratorNormalCompletion2 && _iterator2['return']) {
-        _iterator2['return']();
-      }
+        for (var _iterator2 = fct[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+            var f = _step2.value;
+            f(element);
+        }
+    } catch (err) {
+        _didIteratorError2 = true;
+        _iteratorError2 = err;
     } finally {
-      if (_didIteratorError2) {
-        throw _iteratorError2;
-      }
+        try {
+            if (!_iteratorNormalCompletion2 && _iterator2['return']) {
+                _iterator2['return']();
+            }
+        } finally {
+            if (_didIteratorError2) {
+                throw _iteratorError2;
+            }
+        }
     }
-  }
 }
 
 var MDLCustomAttribut = (function () {
-  function MDLCustomAttribut(element) {
-    _classCallCheck(this, _MDLCustomAttribut);
+    function MDLCustomAttribut(element) {
+        _classCallCheck(this, _MDLCustomAttribut);
 
-    this.element = element;
-  }
-
-  _createClass(MDLCustomAttribut, [{
-    key: 'attached',
-    value: function attached() {
-      //console.log("mdl attached: " + this.value);
-      upgradeElement(this.element, this.value);
+        this.element = element;
     }
-  }]);
 
-  var _MDLCustomAttribut = MDLCustomAttribut;
-  MDLCustomAttribut = (0, _aureliaFramework.customAttribute)('mdl')(MDLCustomAttribut) || MDLCustomAttribut;
-  MDLCustomAttribut = (0, _aureliaFramework.inject)(Element)(MDLCustomAttribut) || MDLCustomAttribut;
-  return MDLCustomAttribut;
+    _createClass(MDLCustomAttribut, [{
+        key: 'attached',
+        value: function attached() {
+            //console.log("mdl attached: " + this.value);
+            upgradeElement(this.element, this.value);
+        }
+    }]);
+
+    var _MDLCustomAttribut = MDLCustomAttribut;
+    MDLCustomAttribut = (0, _aureliaFramework.customAttribute)('mdl')(MDLCustomAttribut) || MDLCustomAttribut;
+    MDLCustomAttribut = (0, _aureliaFramework.inject)(Element)(MDLCustomAttribut) || MDLCustomAttribut;
+    return MDLCustomAttribut;
 })();
 
 exports.MDLCustomAttribut = MDLCustomAttribut;
